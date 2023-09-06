73a78fe3a8121281be04c8a390f7963aba3eee99
flowerjazmin <98027537+flowerjazmin@users.noreply.github.com>
2023-09-06 14:05:21 +0200

    Add files via upload

---
+++ Darusi Jazmin/elso/elso/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace elso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int szam = -6;
+            Console.WriteLine(szam);
+
+            double tort = 12.5;
+            Console.WriteLine(tort);
+
+            char betu = '?';
+            Console.WriteLine(betu);
+            Console.WriteLine(betu+2);
+
+            char a = 'a';
+            Console.WriteLine((char)(a + 2));
+            Console.WriteLine(10 / 3.0);
+
+            int i = 0;
+            Console.WriteLine(++i);
+        }
+    }
+}
