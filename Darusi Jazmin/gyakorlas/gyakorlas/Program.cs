1654c942e3bd046cee19a62ccc86ca3881c1b5b8
flowerjazmin <98027537+flowerjazmin@users.noreply.github.com>
2023-09-19 11:25:58 +0200

    Add files via upload

--- Darusi Jazmin/gyakorlas/gyakorlas/Program.cs
+++ Darusi Jazmin/gyakorlas/gyakorlas/Program.cs
@@ -10,6 +10,7 @@ namespace gyakorlas
     {
         static void Main(string[] args)
         {
+            /*
             int szam = 10000001;
             while (szam<0 || szam>=1000000)
             {
@@ -104,6 +105,170 @@ namespace gyakorlas
             }
             Console.WriteLine("Ennyiszer kell összeadni: " + (szam6 - 1));
 
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            int szamm = 0;
+            while (szamm<=100 || szamm>999)
+            {
+                Console.Write("Kérek egy újabb számot: ");
+                szamm = Convert.ToInt32(Console.ReadLine());
+            }
+
+
+            for (int i = 2; i <= szamm/2 ; i++)
+            {
+                if (szamm % i == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            int szamm1 = 0;
+            int szamm2 = 0;
+            int nagyobb = 0;
+            while (szamm1 <= 100 || szamm1 > 999)
+            {
+                Console.Write("Kérem az első számot: ");
+                szamm1 = Convert.ToInt32(Console.ReadLine());
+            }
+
+            while (szamm2 <= 100 || szamm2 > 999)
+            {
+                Console.Write("Kérem a második számot: ");
+                szamm2 = Convert.ToInt32(Console.ReadLine());
+            }
+
+            if (szamm1 > szamm2)
+            {
+                nagyobb = szamm1;
+            }
+            else 
+            {
+                nagyobb = szamm2;
+            }
+
+            for (int i = 2; i <= nagyobb / 2; i++)
+            {
+                if (szamm1 % i == 0 && szamm2%i==0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            */
+
+            int oszto = 0;
+            Console.Write("Kérek egy számot: ");
+            oszto = Convert.ToInt32(Console.ReadLine());
+
+            /*
+            for (int i = 10000; i <= 99999; i++)
+            {
+                if (i % oszto == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int i = -99999; i <= -10000; i++)
+            {
+                if (i % oszto == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int i = 99999; i >= 10000; i--)
+            {
+                if (i % oszto == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int i = -10000; i >= -99999; i--)
+            {
+                if (i % oszto == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            */
+
+            int tizedik = 0;
+            for (int i = 10000; i <= 99999; i++)
+            {
+                if (i % oszto == 0)
+                {
+                    Console.Write(i + "\t");
+                    tizedik++;
+                    if (tizedik % 10==0)
+                    {
+                        Console.WriteLine();
+                    }
+                }
+            }
+            
+
+            for (int i = -99999; i <= -10000; i++)
+            {
+                if (i % oszto == 0)
+                {
+                    Console.Write(i + "\t");
+                    tizedik++;
+                    if (tizedik % 10 == 0)
+                    {
+                        Console.WriteLine();
+                    }
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int i = 99999; i >= 10000; i--)
+            {
+                if (i % oszto == 0)
+                {
+                    Console.Write(i + "\t");
+                    tizedik++;
+                    if (tizedik % 10 == 0)
+                    {
+                        Console.WriteLine();
+                    }
+                }
+            }
+
+            for (int i = -10000; i >= -99999; i--)
+            {
+                if (i % oszto == 0)
+                {
+                    Console.Write(i + "\t");
+                    tizedik++;
+                    if (tizedik % 10 == 0)
+                    {
+                        Console.WriteLine();
+                    }
+                }
+            }
+            
         }
     }
 }
