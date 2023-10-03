a4ac150e7f120a7e58d799ac97ab680e06e9d757
flowerjazmin <98027537+flowerjazmin@users.noreply.github.com>
2023-10-03 09:39:46 +0200

    Add files via upload

---
+++ valto/valto/Program.cs
@@ -0,0 +1,32 @@
+﻿bool jo = false;
+
+while (!jo)
+{
+    jo = true;
+
+    Console.Write("Mit váltsak át? (pl. 12 kg): ");
+    string beker = Console.ReadLine();
+
+
+
+    string[] darab = beker.Split(" ");
+    if (darab.Length != 2)
+    {
+        jo = false;
+        continue;
+    }
+
+    double szam = 0;
+    try
+    {
+        szam = Convert.ToDouble(darab[0]);
+    }
+    catch (Exception)
+    {
+        Console.WriteLine("Nem jó formátum!");
+        jo = false;
+        continue;
+    }
+
+    Console.WriteLine(szam);
+}
