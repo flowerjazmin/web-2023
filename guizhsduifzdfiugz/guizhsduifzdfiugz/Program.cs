f3d4287a5729524f18671a9afe711f0c687007a5
flowerjazmin <98027537+flowerjazmin@users.noreply.github.com>
2023-10-13 13:19:00 +0200

    Add files via upload

--- guizhsduifzdfiugz/guizhsduifzdfiugz/Program.cs
+++ guizhsduifzdfiugz/guizhsduifzdfiugz/Program.cs
@@ -33,7 +33,7 @@ kiir(szamok);
 */
 
 using System.ComponentModel.DataAnnotations;
-
+/*
 Console.Write("Hány sor legyen?: ");
 int sor = Convert.ToInt32(Console.ReadLine());
 
@@ -74,4 +74,105 @@ for (int i = 0; i < tabla.GetLength(0); i++)
         Console.Write("{0,5} ", tabla[i, k]);
     }
     Console.WriteLine();
+}
+*/
+
+/*
+Console.Write("Kérek egy számot: ");
+int szam = Convert.ToInt32(Console.ReadLine());
+
+string[] tabla= new string[szam];
+
+for (int i = 0;i < tabla.Length; i++)
+{
+    Console.Write("Kérek egy szót: ");
+    tabla[i] = Console.ReadLine();
+}
+
+string max = "";
+
+for (int i = 0; i < tabla.Length; i++)
+{
+    if (tabla[i].Length > max.Length)
+    {
+        max = tabla[i];
+    }
+}
+
+Console.WriteLine("A leghosszabb elem: {0}",max);
+
+
+//index keresés
+int maxIndex = 0;
+for (int i = 0; i < tabla.Length; i++)
+{
+    if (tabla[i] == max)
+    {
+        maxIndex = i;
+    }
+}
+
+//máshogy
+max = "";
+maxIndex=0;
+for (int i = 0; i < tabla.Length; i++)
+{
+    if (tabla[i].Length > max.Length)
+    {
+        max = tabla[i];
+        maxIndex = i;
+    }
+}
+
+
+//megen mashogy
+
+maxIndex = 0;
+for (int i = 0; i < tabla.Length; i++)
+{
+    if (tabla[i].Length > tabla[maxIndex].Length)
+    {
+        maxIndex = i;
+    }
+}
+*/
+/*
+string[] szavak = new string[] { "alma", "körte", "narancs", "mandula", "ananász", "banán" };
+
+string[] rovid = new string[szavak.Length];
+string[] hosszu = new string[szavak.Length];
+
+int rovidIndex = 0;
+int hosszuIndex = 0;
+
+for (int i = 0; i < szavak.Length; i++)
+{
+    if (szavak[i].Length <= 5)
+    {
+        rovid[rovidIndex] = szavak[i];
+        rovidIndex++;
+    }
+    else
+    {
+        hosszu[hosszuIndex++] = szavak[i];
+    }
+}
+
+for (int i = 0;i < rovid.Length;i++)
+{
+    Console.WriteLine(rovid[i]);
+}
+
+for (int i = 0; i < hosszu.Length; i++)
+{
+    Console.WriteLine(hosszu[i]);
+}
+*/
+
+int[] szamok = new int[1000];
+
+for (int i = 0; i < szamok.Length; i++)
+{
+    Random rand = new Random();
+    rand.Next(99,1000);
 }
